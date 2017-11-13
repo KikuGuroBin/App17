173d57dfb0fbfff42bd6df2615d27a1394a53e35
takumi <ru.taku.i.1220@gmail.com>
2017-11-13 14:01:08 +0900

    ブランチの追加について

---
+++ App17/App17.iOS/MyImageRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using App17;
+using App17.iOS;
+
+[assembly:ExportRenderer(typeof(MyImage), typeof(MyImageRenderer))]
+namespace App17.iOS
+{
+    public class MyImageRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
